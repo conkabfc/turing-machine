39406c7848eeeb73ab4b26e287f717a892d14937
Connor Whitworth <connorwhitworth4@gmail.com>
2023-10-05 20:32:32 +0100

    TUR-00: Set up files

---
+++ turing.py
@@ -0,0 +1,7 @@
+import turing_machine
+
+def turing():
+    rules = ''
+    states = ['','']
+    tape = ''
+    turing_machine(rules, states, tape)
---
+++ turing_machine.py
@@ -0,0 +1,2 @@
+def turing_machine(rules, states, tape):
+    return 0
