07bbaf0cd37fc61f88ae09ffc17b4415b27ec0f5
Connor Whitworth <connorwhitworth4@gmail.com>
2023-10-13 15:56:53 +0100

    TUR-01 Accepts input for rules, states, tape, t-o

--- turing.py
+++ turing.py
@@ -1,7 +1,24 @@
 import turing_machine
 
-def turing():
-    rules = ''
-    states = ['','']
-    tape = ''
-    turing_machine(rules, states, tape)
+def main():
+    print('Enter a list of rules as a string - in the format\n<s,t,s,t,X>,<s,t,s,t,X>,... where s is a state, t\nis a tape symbol, and X is one of L, R, P')
+    try:
+        rules = turing_machine.parse_rules(input())
+    except:
+        print('Rules are in invalid format.')
+        return
+    print('Enter a list of possible states - in the format\na,b,c,d,..,z - ie each state separated by a comma')
+    try:
+        states = input().split(',')
+    except:
+        print('States are in invalid format.')
+    print('Enter the starting tape, with an underscore for blanks.')
+    tape = input()
+    print('Enter time in ms for computation timeout')
+    try:
+        timeout = int(input())
+    except:
+        timeout = 5000
+    turing_machine.turing_machine(rules, states, tape, timeout)
+
+main()
